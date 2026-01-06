014c7a3e697c8cd04ed2701cb94b3f1e5e842767
Ayake <golly.ticker@gmail.com>
2026-01-06 18:54:02 +0100

    .

--- test/bridge.needs-backend.integration-tests.sh
+++ test/bridge.needs-backend.integration-tests.sh
@@ -8,4 +8,9 @@ source test/source.sh
 
 echo "! Test assumes that pluralsync-api is running !"
 
+echo "TODO: fix me. Ensure that window is opened into the foreground for the test!"
+exit 1
+
+# also bug on windows icon not being shown: https://github.com/tauri-apps/tauri/issues/2692
+
 (cd bridge-frontend && npm run e2e)
